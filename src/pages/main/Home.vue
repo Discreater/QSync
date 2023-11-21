2003caa931d0437eaf7b6f761ab673eecd257177
Discreater <discreater@163.com>
2023-11-22 01:20:07 +0800

    feat: Update Home

--- src/pages/main/Home.vue
+++ src/pages/main/Home.vue
@@ -6,5 +6,11 @@ const { t } = useI18n();
 </script>
 
 <template>
-  <Basic :header="t('menu.home')" />
+  <Basic :header="t('menu.home')">
+    - English translation is not complete yet. Nothing here.
+    <br>
+    - 主页是空的，因为我懒得做。你需要以 `Kerse` 账号登陆，登录之后可能还要重启应用 :(。
+    <br>
+    - 为了有歌听，然后你还要在 `源` 页面中添加本地音乐文件夹。
+  </Basic>
 </template>
